1c407fc79ee2c95e212ee5bd8bfaea2d2aab27d6
koheiM <matsudakohei88@gmail.com>
2021-03-15 11:31:49 +0900

    make preview for projects/index

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -15,7 +15,6 @@ export const Index = ({ allPosts, preview }) => {
     <>
       <Header />
       {preview && <PreviewMode />}
-      
         {allPosts.map((post) => {
           return (
             <BlogPost
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,31 +1,43 @@
+import { useRouter } from "next/router";
+import { Loading } from "../../components/loading";
+import { Header } from "../../components/header";
+import { PreviewMode } from "../../components/previewMode";
 import { ProjectPost } from "../../components/projectPost";
-import { getAllProjectPosts } from "../../lib/api"
+import { getAllProjectPosts } from "../../lib/api";
 
-export const Index = ({ allPosts }) => {
+export const Index = ({ allPosts, preview }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <div>
-        {allPosts.map((post) => {
-          return (
-            <ProjectPost
-              p_title={post.node.title}
-              p_image={post.node.image.url}
-              // p_link={post.node.link.url}
-              p_richbody={post.node.richbody}
-              p_uid={post.node._meta.uid}
-              key={post.node._meta.uid}
-            />
-          );
-        })}
-      </div>
+      <Header />
+      {preview && <PreviewMode />}
+      {allPosts.map((post) => {
+        return (
+          <ProjectPost
+            p_title={post.node.title}
+            p_image={post.node.image.url}
+            // p_link={post.node.link.url}
+            p_richbody={post.node.richbody}
+            p_uid={post.node._meta.uid}
+            key={post.node._meta.uid}
+          />
+        );
+      })}
     </>
   );
 };
 
-export async function getStaticProps() {
-  const allPosts = await getAllProjectPosts();
+export async function getStaticProps({ preview = false, previewData }) {
+  const allPosts = await getAllProjectPosts(previewData);
   return {
-    props: { allPosts },
+    props: {
+      preview,
+      allPosts,
+    },
   };
 }
 
