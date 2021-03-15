55aede7dcce2cd9fb952b49a3aba99a960f73a18
koheiM <matsudakohei88@gmail.com>
2021-03-15 10:58:26 +0900

    make preview for posts/index

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,28 +1,41 @@
-import { getAllBlogPosts } from '../../lib/api'
-import { BlogPost } from '../../components/blogPost'
+import { useRouter } from "next/router";
+import { Loading } from "../../components/loading";
+import { Header } from "../../components/header";
+import { PreviewMode } from "../../components/previewMode"
+import { BlogPost } from "../../components/blogPost";
+import { getAllBlogPosts } from "../../lib/api";
 
-export const Index = ({ allPosts }) => {
-  return(
+export const Index = ({ allPosts, preview }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
+  return (
     <>
-      <div>
-      {allPosts.map((post) => {
-        return (
-          <BlogPost
-            p_title={post.node.title}
-            p_date={post.node.date}
-            key={post.node._meta.uid}
-          />
-        );
-      })}
-      </div>
+      <Header />
+      {preview && <PreviewMode />}
+      
+        {allPosts.map((post) => {
+          return (
+            <BlogPost
+              p_title={post.node.title}
+              p_date={post.node.date}
+              key={post.node._meta.uid}
+            />
+          );
+        })}
     </>
-  )
-}
+  );
+};
 
-export async function getStaticProps() {
-  const allPosts = await getAllBlogPosts();
+export async function getStaticProps({ preview=false, previewData }) {
+  const allPosts = await getAllBlogPosts(previewData);
   return {
-    props: { allPosts },
+    props: { 
+      preview,
+      allPosts
+    },
   };
 }
 
