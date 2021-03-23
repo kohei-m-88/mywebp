eb46a176b90c91b6729b8f833954e887cbab3de4
koheiM <matsudakohei88@gmail.com>
2021-03-23 22:38:43 +0900

    add seo posts/index

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,16 @@
+import { PageSeo } from '../../components/seo'
+import siteMetadata from '../../data/siteMetadata'
 import { BlogPost } from '../../components/blogPost'
 import { getAllBlogPosts } from '../../lib/api'
 
 export const Index = ({ allPosts, preview }) => {
   return (
     <>
+      <PageSeo
+        title={`Blog - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+        url={`${siteMetadata.siteUrl}/posts`}
+      />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
